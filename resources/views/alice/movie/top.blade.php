9bae342f2ed20827205733784e0c5204f6f21cc6
EC2 Default User <ec2-user@ip-172-31-36-237.ap-northeast-1.compute.internal>
2025-05-10 09:16:58 +0000

    直し

--- resources/views/alice/movie/top.blade.php
+++ resources/views/alice/movie/top.blade.php
@@ -1,103 +1,41 @@
 <!DOCTYPE html>
-<!--[if IE 7]>
-<html class="ie ie7" lang="ja" prefix=" ">
-<![endif]-->
- <!--[if IE 8]>
- <html class="ie ie8" lang="ja" prefix=" ">
-<![endif]-->
- <!--[if !(IE 7) & !(IE 8)]><!-->
- <html lang="ja" prefix=" " class=""><!--<![endif]--><head>
+ <html lang="ja" prefix=" " class="">
+ <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>アリスの映画紹介サイト | 映画予告編制作</title>
 	<link rel="shortcut icon" href=" ">
 	<link rel="stylesheet" href=" ">
-	
-			<script type="text/javascript" async="" src=" "></script><script type=" " async="" src=" "></script><script async="" src=" "></script><script src=" "></script>
-
-	<!--[if lt IE 9]>
-	<script src=""></script>
-	<script src=""></script>
-	<![endif]-->
-	<!--[if (gte IE 6)&(lte IE 8)]>
-	<script src=""></script>
-	<![endif]-->
-
-	<!-- Google Tag Manager --> //ウェブサイトやアプリにタグを設置・管理するための無料のGoogle提供ツール
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	''+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-T3P5XBB');</script>
-
-	
-<script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>
-<title>アリスの映画紹介サイト | 映画予告編制作</title>
-
-
-<body class="home page-template page-template-page-top page-template-page-top-php page page-id-2">
-	<!-- Google Tag Manager (noscript) -->
-	<noscript><iframe src=""
-	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-	<!-- End Google Tag Manager (noscript) -->
-
-		
+</head>
 	<div id="container">
-		<header id="header" class="delayedFadeIn" style="opacity: 1;"> //要素の不透明度を1に設定する
-			<div class="wrapper"> //ラップ関数
-				<div class="logo">
-					<a href=""><img src="" alt=""></a>
-					<a class="button-open"><i class="fa fa-bars"></i></a> //折り畳み表示
-					<a class="button-close"><i class="fa fa-close"></i></a>
 				</div>
 			</div>
 		</header>
-		<figure id="rotation-logo"> //ロゴを回転させる
-			<img src="" alt="alice movie-intro." width="11"> //タグの横幅
-		</figure>
 		<nav id="nav" class="delayedFadeIn3" role="navigation" style="display: block; opacity: 1;">
 			<ul>
-				<li><a href=""><img src="" alt="TOP" width="11"></a></li>
-				<li><a href=""><img src="" alt="academy" width="11"></a></li>
-				<li><a href=""><img src="" alt="ranking" width="11"></a></li>
-				<li><a href=""><img src="" alt="year" width="11"></a></li>
-				<li><a href=""><img src="" alt="ganle" width="11"></a></li>
+				<li><a href=""><img src="" alt="TOP" ></a></li>
+				<li><a href=""><img src="" alt="academy" ></a></li>
+				<li><a href=""><img src="" alt="ranking" ></a></li>
+				<li><a href=""><img src="" alt="year" ></a></li>
+				<li><a href=""><img src="" alt="ganle" ></a></li>
 			</ul>
-		</nav>
-				<figure id="key" class="delayedFadeIn2" style="opacity: 1;">
-			
+		</nav>		
 				<main role="main">
 			<div id="main">
-
-
 				<section id="top-works">
-					<div class="wrapper">
-						<div class="loops">
-							<div class="row type-works">
-								
-																																										
-																				
-
-                <div class="col x2 match" style="height: 620px;">
-                  <figure class="teaser-image">
-                    <div class="popup">
-                      <a href="" class="thickbox">
-                        <span class="image"><img width="1200" height="630" src="" class="attachment-full size-full wp-post-image" alt="" srcset="</span>
                       </a>
                     </div>
                   </figure>
                   <div class="teaser-title">
                     <span class="meta"></span>
-                    <span class="title">バックインアクション</span>
+                    <span>"title">バックインアクション</span>
                   </div>
                   <div class="teaser-date">
                     <span class="year">2025</span>
                     <span class="date">3</span>
                   </div>
                 </div>
-									
-																				
 
                 <div class="col x2 match" style="height: 620px;">
                   <figure class="teaser-image">
@@ -116,8 +54,6 @@
                   </div>
                 </div>
 									
-																				
-
                 <div class="col x2 match" style="height: 620px;">
                   <figure class="teaser-image">
                     <div class="popup">
@@ -139,10 +75,7 @@
 
                 <div class="col x2 match" style="height: 620px;">
                   <figure class="teaser-image">
-                    <div class="popup">
-                      <a href="http://cirocraft.co.jp/%e3%83%95%e3%82%a7%e3%83%ab%e3%83%a1%e3%83%bc%e3%83%ab-greatest-exhibition/index.php?keepThis=true&amp;TB_iframe=true&amp;height=700&amp;width=954" class="thickbox">
-                        <span class="image"><img width="900" height="506" src="http://cirocraft.co.jp/wp/wp-content/uploads/2023/11/vermeer_thumbnail.jpg" class="attachment-full size-full wp-post-image" alt="" srcset="http://cirocraft.co.jp/wp/wp-content/uploads/2023/11/vermeer_thumbnail.jpg 900w, http://cirocraft.co.jp/wp/wp-content/uploads/2023/11/vermeer_thumbnail-300x169.jpg 300w, http://cirocraft.co.jp/wp/wp-content/uploads/2023/11/vermeer_thumbnail-768x432.jpg 768w" sizes="(max-width: 900px) 100vw, 900px"></span>
-                      </a>
+                    
                     </div>
                   </figure>
                   <div class="teaser-title">
@@ -160,9 +93,7 @@
                 <div class="col x2 match" style="height: 620px;">
                   <figure class="teaser-image">
                     <div class="popup">
-                      <a href="http://cirocraft.co.jp/18%e6%ad%b3%e3%81%ae%e3%81%8a%e3%81%a8%e3%81%aa%e3%81%9f%e3%81%a1/index.php?keepThis=true&amp;TB_iframe=true&amp;height=700&amp;width=954" class="thickbox">
-                        <span class="image"><img width="1920" height="1080" src="http://cirocraft.co.jp/wp/wp-content/uploads/2023/11/18sai.png" class="attachment-full size-full wp-post-image" alt="" srcset="http://cirocraft.co.jp/wp/wp-content/uploads/2023/11/18sai.png 1920w, http://cirocraft.co.jp/wp/wp-content/uploads/2023/11/18sai-300x169.png 300w, http://cirocraft.co.jp/wp/wp-content/uploads/2023/11/18sai-768x432.png 768w, http://cirocraft.co.jp/wp/wp-content/uploads/2023/11/18sai-1024x576.png 1024w" sizes="(max-width: 1920px) 100vw, 1920px"></span>
-                      </a>
+                      
                     </div>
                   </figure>
                   <div class="teaser-title">
@@ -179,10 +110,7 @@
 
                 <div class="col x2 match" style="height: 620px;">
                   <figure class="teaser-image">
-                    <div class="popup">
-                      <a href="http://cirocraft.co.jp/%e6%98%a0%e7%94%bb%e3%80%8e%e9%a6%96%e3%80%8f/index.php?keepThis=true&amp;TB_iframe=true&amp;height=700&amp;width=954" class="thickbox">
-                        <span class="image"><img width="1280" height="720" src="http://cirocraft.co.jp/wp/wp-content/uploads/2023/05/kubi.jpg" class="attachment-full size-full wp-post-image" alt="" srcset="http://cirocraft.co.jp/wp/wp-content/uploads/2023/05/kubi.jpg 1280w, http://cirocraft.co.jp/wp/wp-content/uploads/2023/05/kubi-300x169.jpg 300w, http://cirocraft.co.jp/wp/wp-content/uploads/2023/05/kubi-768x432.jpg 768w, http://cirocraft.co.jp/wp/wp-content/uploads/2023/05/kubi-1024x576.jpg 1024w" sizes="(max-width: 1280px) 100vw, 1280px"></span>
-                      </a>
+                   
                     </div>
                   </figure>
                   <div class="teaser-title">
@@ -199,11 +127,7 @@
 
                 <div class="col x2 match" style="height: 620px;">
                   <figure class="teaser-image">
-                    <div class="popup">
-                      <a href="http://cirocraft.co.jp/%e6%98%a0%e7%94%bb-%e3%83%96%e3%83%a9%e3%83%83%e3%82%af%e3%82%af%e3%83%ad%e3%83%bc%e3%83%90%e3%83%bc-%e9%ad%94%e6%b3%95%e5%b8%9d%e3%81%ae%e5%89%a3/index.php?keepThis=true&amp;TB_iframe=true&amp;height=700&amp;width=954" class="thickbox">
-                        <span class="image"><img width="1280" height="720" src="http://cirocraft.co.jp/wp/wp-content/uploads/2022/10/maxresdefault-1.jpg" class="attachment-full size-full wp-post-image" alt="" srcset="http://cirocraft.co.jp/wp/wp-content/uploads/2022/10/maxresdefault-1.jpg 1280w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/10/maxresdefault-1-300x169.jpg 300w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/10/maxresdefault-1-768x432.jpg 768w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/10/maxresdefault-1-1024x576.jpg 1024w" sizes="(max-width: 1280px) 100vw, 1280px"></span>
-                      </a>
-                    </div>
+                    
                   </figure>
                   <div class="teaser-title">
                     <span class="meta">エイベックス・ピクチャーズ</span>
@@ -220,9 +144,6 @@
                 <div class="col x2 match" style="height: 620px;">
                   <figure class="teaser-image">
                     <div class="popup">
-                      <a href="http://cirocraft.co.jp/ghostbook-%e3%81%8a%e3%81%b0%e3%81%91%e3%81%9a%e3%81%8b%e3%82%93/index.php?keepThis=true&amp;TB_iframe=true&amp;height=700&amp;width=954" class="thickbox">
-                        <span class="image"><img width="1920" height="1080" src="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/ghostbook02.jpg" class="attachment-full size-full wp-post-image" alt="" srcset="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/ghostbook02.jpg 1920w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/ghostbook02-300x169.jpg 300w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/ghostbook02-768x432.jpg 768w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/ghostbook02-1024x576.jpg 1024w" sizes="(max-width: 1920px) 100vw, 1920px"></span>
-                      </a>
                     </div>
                   </figure>
                   <div class="teaser-title">
@@ -240,9 +161,7 @@
                 <div class="col x2 match" style="height: 620px;">
                   <figure class="teaser-image">
                     <div class="popup">
-                      <a href="http://cirocraft.co.jp/%e3%83%90%e3%82%b9%e3%82%ab%e3%83%b4%e3%82%a3%e3%83%ab%e5%ae%b6%e3%81%ae%e7%8a%ac-%e3%82%b7%e3%83%a3%e3%83%bc%e3%83%ad%e3%83%83%e3%82%af%e5%8a%87%e5%a0%b4%e7%89%88/index.php?keepThis=true&amp;TB_iframe=true&amp;height=700&amp;width=954" class="thickbox">
-                        <span class="image"><img width="1280" height="720" src="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/baskervilles.jpg" class="attachment-full size-full wp-post-image" alt="" srcset="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/baskervilles.jpg 1280w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/baskervilles-300x169.jpg 300w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/baskervilles-768x432.jpg 768w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/baskervilles-1024x576.jpg 1024w" sizes="(max-width: 1280px) 100vw, 1280px"></span>
-                      </a>
+                      
                     </div>
                   </figure>
                   <div class="teaser-title">
@@ -255,54 +174,6 @@
                   </div>
                 </div>
 									
-																				
-
-                <div class="col x2 match" style="height: 620px;">
-                  <figure class="teaser-image">
-                    <div class="popup">
-                      <a href="http://cirocraft.co.jp/kappei/index.php?keepThis=true&amp;TB_iframe=true&amp;height=700&amp;width=954" class="thickbox">
-                        <span class="image"><img width="1280" height="720" src="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0318_KAPPEI.jpg" class="attachment-full size-full wp-post-image" alt="" srcset="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0318_KAPPEI.jpg 1280w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0318_KAPPEI-300x169.jpg 300w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0318_KAPPEI-768x432.jpg 768w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0318_KAPPEI-1024x576.jpg 1024w" sizes="(max-width: 1280px) 100vw, 1280px"></span>
-                      </a>
-                    </div>
-                  </figure>
-                  <div class="teaser-title">
-                    <span class="meta">東宝</span>
-                    <span class="title">KAPPEI</span>
-                  </div>
-                  <div class="teaser-date">
-                    <span class="year">2022</span>
-                    <span class="date">3/18</span>
-                  </div>
-                </div>
-									
-																				
-
-                <div class="col x2 match" style="height: 620px;">
-                  <figure class="teaser-image">
-                    <div class="popup">
-                      <a href="http://cirocraft.co.jp/%e3%83%89%e3%83%aa%e3%83%bc%e3%83%a0%e3%83%97%e3%83%a9%e3%83%b3/index.php?keepThis=true&amp;TB_iframe=true&amp;height=700&amp;width=954" class="thickbox">
-                        <span class="image"><img width="1280" height="720" src="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0223_DREAMPLAN.jpg" class="attachment-full size-full wp-post-image" alt="" srcset="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0223_DREAMPLAN.jpg 1280w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0223_DREAMPLAN-300x169.jpg 300w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0223_DREAMPLAN-768x432.jpg 768w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/22_0223_DREAMPLAN-1024x576.jpg 1024w" sizes="(max-width: 1280px) 100vw, 1280px"></span>
-                      </a>
-                    </div>
-                  </figure>
-                  <div class="teaser-title">
-                    <span class="meta">ワーナー ブラザース</span>
-                    <span class="title">ドリームプラン</span>
-                  </div>
-                  <div class="teaser-date">
-                    <span class="year">2022</span>
-                    <span class="date">2/23</span>
-                  </div>
-                </div>
-									
-																				
-
-                <div class="col x2 match" style="height: 620px;">
-                  <figure class="teaser-image">
-                    <div class="popup">
-                      <a href="http://cirocraft.co.jp/%e6%ad%bb%e9%9c%8a%e9%a4%a8-%e6%82%aa%e9%ad%94%e3%81%ae%e3%81%9b%e3%81%84%e3%81%aa%e3%82%89%e3%80%81%e7%84%a1%e7%bd%aa%e3%80%82/index.php?keepThis=true&amp;TB_iframe=true&amp;height=700&amp;width=954" class="thickbox">
-                        <span class="image"><img width="1280" height="720" src="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/21_1001_shiryoukan.jpg" class="attachment-full size-full wp-post-image" alt="" srcset="http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/21_1001_shiryoukan.jpg 1280w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/21_1001_shiryoukan-300x169.jpg 300w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/21_1001_shiryoukan-768x432.jpg 768w, http://cirocraft.co.jp/wp/wp-content/uploads/2022/01/21_1001_shiryoukan-1024x576.jpg 1024w" sizes="(max-width: 1280px) 100vw, 1280px"></span>
-                      </a>
                     </div>
                   </figure>
                   <div class="teaser-title">
@@ -314,99 +185,4 @@
                     <span class="date">10/1</span>
                   </div>
                 </div>
-																		
-																								
-
-							</div><!-- /.row -->
-						</div><!-- /.loops -->
-
-						<div class="more-works">
-							<a class="over" href="http://cirocraft.co.jp/works"><img src="http://cirocraft.co.jp/wp/wp-content/themes/cirocraft/images/button-more-works.png" width="180" alt="More Works"></a>
-						</div>
-					</div><!-- /.wrapper -->
-				</section>
-
-
-				<section id="profile">
-					<div class="wrapper">
-						<div class="cnt-l">
-							<h2 class="hd-main">PROFILE</h2>
-							<table>
-								
-							</tbody></table>
-						</div>
-						<div class="cnt-r">
-							<!-- <div id="map_canvas" style="width: 100%; height:400px;"></div> -->
-							<iframe src="</iframe>
-						</div>
-					</div><!-- /.wrapper -->
-				</section>
-
-
-		</div><!-- / #main -->
-		</main><!-- / main -->
-		<footer>
-			<div id="footer">
-				<div class="wrapper">
-					<nav class="nav">
-						<ul>
-							
-						</ul>
-					</nav>
-					<div class="copyright">
-						<small>© 2023 CIROCRAFT INC.</small>
-					</div>
-				</div>
-			</div>
-		</footer>
-	</div><!--/ #container -->
-
-	<script type="text/javascript" src="http://cirocraft.co.jp/wp/wp-content/themes/cirocraft/js/lib/jquery.easing.js"></script>
-	<script type="text/javascript" src="http://cirocraft.co.jp/wp/wp-content/themes/cirocraft/js/script.js"></script>
-
-	<script type="text/javascript" src="http://cirocraft.co.jp/wp/wp-content/themes/cirocraft/js/lib/thickbox/thickbox.js"></script>
-	<link rel="stylesheet" href="http://cirocraft.co.jp/wp/wp-content/themes/cirocraft/js/lib/thickbox/thickbox.css" type="text/css" media="all">
-
-	<!-- matchHeight -->
-	<script type="text/javascript" src="http://cirocraft.co.jp/wp/wp-content/themes/cirocraft/js/lib/jquery.matchHeight.js"></script>
-	<script type="text/javascript">
-	$(function() {
-	    $('.match').matchHeight();
-	});
-	</script>
-
-	
-		
-		
-
-<script type="text/javascript" src="http://cirocraft.co.jp/wp/wp-content/themes/twentyfifteen/js/skip-link-focus-fix.js"></script>
-<script type="text/javascript" src="http://cirocraft.co.jp/wp/wp-includes/js/wp-embed.min.js"></script>
-
-
 </body></html>
-<!--<![endif]-->
-<head></head>
-<body>
-	<!-- Google Tag Manager (noscript) -->
-	<!-- End Google Tag Manager (noscript) -->
-	<noscript></noscript>
-	<div></div>
-	<header>
-		<div>
-        </div>
-	</header>
-		<figure>
-		</figure>
-		<nav>
-		</nav>
-		<figure>
-		</figure>
-		<main></mein>
-<!-- / main -->
- <footer></footer>
- <div>
-<!--/ #container -->
- </div>
- <script></script>
-</body>
-</html>
