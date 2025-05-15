a19c7e851af8e4d2bd38530e6de71c4e9b10c489
EC2 Default User <ec2-user@ip-172-31-36-237.ap-northeast-1.compute.internal>
2025-05-15 09:19:46 +0000

    page

--- resources/views/alice/movie/top.blade.php
+++ resources/views/alice/movie/top.blade.php
@@ -22,35 +22,73 @@
 				<li><a href="#">year</a></li>
 				<li><a href="#">ganle</a></li>
 			</ul>		
-				<section id="top-works">
-                      </a>
-                    </div>
-                  </figure>
-                  <div class="teaser-title">
-                    <span class="meta"></span>
-                    <span>"title">バックインアクション</span>
-                  </div>
-                  <div class="teaser-date">
-                    <span class="year">2025</span>
-                    <span class="date">3</span>
-                  </div>
-                </div>
+    </div>
 
-                
-                  <figure class="teaser-image">
-                    <div class="popup">
-                      <a href="</span>
-                      </a>
-                    </div>
-                  </figure>
-                  <div class="teaser-title">
-                    <span class="meta">東宝</span>
-                    <span class="title">スマホを落としただけなのに ～最終章～ ファイナルハッキングゲーム</span>
-                  </div>
-                  <div class="teaser-date">
-                    <span class="year">2024</span>
-                    <span class="date"></span>
-                  </div>
+    <div class="main">
+     <div class="container">
+       <div class="left-contents">
+          <div class="card-contents">
+            <div class="movie-list">
+              <img src="./image/staff_01.jpg" class="staff-image">
+              <h2 class="text-title">バックインアクション</h2>
+            </div>
+              <div class="teaser-date">
+                <span class="year">2025</span>
+                <span class="date">1/17</span>
+              </div>
+          </div>  
+
+            <div class="card-contents">
+              <div class="teaser-title">
+                <img src="./image/image2" class="staff-image">
+                <h2 class="text-title">ミステリというなかれ</h2>
+              </div>
+              <div class="teaser-date">
+                <span class="year">2023</span>
+                <span class="date">9/15</span>
+              </div>
+
+            <div class="card-contents">
+            <div class="teaser-title">
+            <img src="./image/image3" class="staff-image">
+              <h2 class="text-title">ジュラシックワールド</h2>
+            </div>
+              <span class="year">2015</span>
+              <span class="date">8/5</span>
+
+          <div class="card-contents">
+          <div class="teaser-title">
+          <img src="./image/image4" class="staff-image">
+            <h2 class="text-title">翔んで埼玉</h2>
+          </div>
+              <span class="year">2019</span>
+              <span class="date">2/22</span>
+
+          <div class="card-contents">
+          <div class="teaser-title">
+          <img src="./image/image5" class="staff-image">
+            <h2 class="text-title">死霊館</h2>
+          </div>
+            <span class="year">2019</span>
+            <span class="date">2/22</span>
+
+
+            <div class="card-contents">
+            <div class="teaser-title">
+              <img src="./image/image6" class="staff-image">
+              <h2 class="text-title">シンデレラ</h2>
+            </div>
+            <div class="teaser-date">
+              <span class="year">2015</span>
+              <span class="date">4/25</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+				<section id="top-works">
+            
 									
                 <div class="col x2 match" style="height: 620px;">
                   <figure class="teaser-image">
@@ -123,10 +161,7 @@
 									
 																				
 
-                <div class="col x2 match" style="height: 620px;">
-                  <figure class="teaser-image">
-                    
-                  </figure>
+                
                   <div class="teaser-title">
                     <span class="meta">エイベックス・ピクチャーズ</span>
                     <span class="title">映画 ブラッククローバー 魔法帝の剣</span>
@@ -136,51 +171,5 @@
                     <span class="date">6/16</span>
                   </div>
                 </div>
-									
-																				
-
-                <div class="col x2 match" style="height: 620px;">
-                  <figure class="teaser-image">
-                    <div class="popup">
-                    </div>
-                  </figure>
-                  <div class="teaser-title">
-                    <span class="meta">東宝</span>
-                    <span class="title">GHOSTBOOK おばけずかん</span>
-                  </div>
-                  <div class="teaser-date">
-                    <span class="year">2022</span>
-                    <span class="date">7/22</span>
-                  </div>
-                </div>
-									
-																				
-
-                <div class="col x2 match" style="height: 620px;">
-                  <figure class="teaser-image">
-                    <div class="popup">
-                      
-                    </div>
-                  </figure>
-                  <div class="teaser-title">
-                    <span class="meta">東宝</span>
-                    <span class="title">バスカヴィル家の犬 シャーロック劇場版</span>
-                  </div>
-                  <div class="teaser-date">
-                    <span class="year">2022</span>
-                    <span class="date">6/17</span>
-                  </div>
-                </div>
-									
-                    </div>
-                  </figure>
-                  <div class="teaser-title">
-                    <span class="meta">ワーナー ブラザース</span>
-                    <span class="title">死霊館 悪魔のせいなら、無罪。</span>
-                  </div>
-                  <div class="teaser-date">
-                    <span class="year">2021</span>
-                    <span class="date">10/1</span>
-                  </div>
-                </div>
+                    
 </body></html>
