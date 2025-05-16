a845c9af6eb8e7f44a7b7e84a79ad4fe1979e5b0
EC2 Default User <ec2-user@ip-172-31-36-237.ap-northeast-1.compute.internal>
2025-05-16 11:22:15 +0000

    page

--- resources/views/alice/movie/top.blade.php
+++ resources/views/alice/movie/top.blade.php
@@ -1,95 +1,91 @@
 <!DOCTYPE html>
- <html>
- <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>アリスの映画紹介サイト | 映画予告編制作</title>
-    <link rel="shortcut icon" href=" ">
-    <link rel="stylesheet" href=" ">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/style.css">
+<html>
+  <head>
+      <meta charset="UTF-8">
+      <title>アリスの映画紹介サイト | 映画予告編制作</title>
+      <link rel="stylesheet" href="css/normalize.css">
+      <link rel="stylesheet" href="css/style.css">
   </head>
-<body>
-  <header>
-	  <div class="container">
-      <div class="header-title-area">
-      <h1 class="logo">アリスの映画紹介サイト</h1>
-     </div>             
-			<ul class="header-navigation">
-				<li><a href="#">TOP</a></li>
-				<li><a href="#">ranking</a></li>
-				<li><a href="#">year</a></li>
-				<li><a href="#">ganle</a></li>
-			</ul>		
-    </div>
-  <header>
-    <div class="main">
-     <div class="container">
-       <div class="left-contents">
-          <div class="card-contents">
-              <div class="movie-list">
-                <img src="./image/staff_01.jpg" class="staff-image">
-                <h2 class="text-title">バックインアクション</h2>
-              </div>
-                <div class="teaser-date">
-                  <span class="year">2025</span>
-                  <span class="date">1/17</span>
-                </div>
-            </div> 
-
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-title-area">
+          <h1 class="logo">アリスの映画紹介サイト</h1>
+        </div>             
+        <ul class="header-navigation">
+          <li><a href="#">TOP</a></li>
+          <li><a href="#">ranking</a></li>
+          <li><a href="#">year</a></li>
+          <li><a href="#">ganle</a></li>
+        </ul>		
+      </div>
+    <header>
+      <div class="main">
+      <div class="container">
+        <div class="left-contents">
             <div class="card-contents">
-              <div class="teaser-title">
-                <img src="./image/image2" class="staff-image">
-                <h2 class="text-title">ミステリというなかれ</h2>
-              </div>
-              <div class="teaser-date">
-                <span class="year">2023</span>
-                <span class="date">9/15</span>
-              </div>
-            </div>
+                <div class="movie-list">
+                  <img src="./image/image" class="">
+                  <h2 class="text-title">バックインアクション</h2>
+                </div>
+                  <div class="teaser-date">
+                    <span class="year">2025</span>
+                    <span class="date">1/17</span>
+                  </div>
+              </div> 
 
               <div class="card-contents">
                 <div class="teaser-title">
-                  <img src="./image/image3" class="staff-image">
-                  <h2 class="text-title">ジュラシックワールド</h2>
+                  <img src="./image/image2" class="">
+                  <h2 class="text-title">ミステリというなかれ</h2>
+                </div>
+                <div class="teaser-date">
+                  <span class="year">2023</span>
+                  <span class="date">9/15</span>
+                </div>
+              </div>
+
+                <div class="card-contents">
+                  <div class="teaser-title">
+                    <img src="./image/image3" class="">
+                    <h2 class="text-title">ジュラシックワールド</h2>
+                  </div>
+                    <span class="year">2015</span>
+                    <span class="date">8/5</span>
+                </div>
+                  
+
+              <div class="right-contents">
+                <div class="card-contents">
+                  <div class="teaser-title">
+                    <img src="./image/image4" class="">
+                      <h2 class="text-title">翔んで埼玉</h2>
+                  </div>
+                  <span class="year">2019</span>
+                  <span class="date">2/22</span>
+              </div>
+
+                <div class="card-contents">
+                <div class="teaser-title">
+                <img src="./image/image5" class="">
+                  <h2 class="text-title">死霊館</h2>
+                </div>
+                  <span class="year">2019</span>
+                  <span class="date">2/22</span>
+                </div>
+
+                <div class="card-contents">
+                <div class="teaser-title">
+                  <img src="./image/image6" class="">
+                  <h2 class="text-title">シンデレラ</h2>
                 </div>
                   <span class="year">2015</span>
-                  <span class="date">8/5</span>
-              </div>
-                
-
-            <div class="right-contents">
-              <div class="card-contents">
-                <div class="teaser-title">
-                  <img src="./image/image4" class="staff-image">
-                    <h2 class="text-title">翔んで埼玉</h2>
+                  <span class="date">4/25</span>
                 </div>
-                <span class="year">2019</span>
-                <span class="date">2/22</span>
-             </div>
-
-              <div class="card-contents">
-               <div class="teaser-title">
-              <img src="./image/image5" class="staff-image">
-                <h2 class="text-title">死霊館</h2>
               </div>
-                <span class="year">2019</span>
-                <span class="date">2/22</span>
-              </div>
-
-              <div class="card-contents">
-              <div class="teaser-title">
-                <img src="./image/image6" class="staff-image">
-                <h2 class="text-title">シンデレラ</h2>
-              </div>
-                <span class="year">2015</span>
-                <span class="date">4/25</span>
-              </div>
-            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   </body>
 </html>
