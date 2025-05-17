f65f0479c5830842e21eb7dee730d4c19d35cf89
EC2 Default User <ec2-user@ip-172-31-36-237.ap-northeast-1.compute.internal>
2025-05-17 09:29:53 +0000

    page

--- resources/views/alice/movie/top.blade.php
+++ resources/views/alice/movie/top.blade.php
@@ -1,91 +1,112 @@
 <!DOCTYPE html>
 <html>
   <head>
-      <meta charset="UTF-8">
-      <title>アリスの映画紹介サイト | 映画予告編制作</title>
-      <link rel="stylesheet" href="css/normalize.css">
-      <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <title>アリスの映画紹介サイト | 映画予告編制作</title>
+    <link rel="stylesheet" href="{{ asset('css/normalize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   </head>
   <body>
     <header>
-      <div class="container">
-        <div class="header-title-area">
-          <h1 class="logo">アリスの映画紹介サイト</h1>
-        </div>             
-        <ul class="header-navigation">
-          <li><a href="#">TOP</a></li>
-          <li><a href="#">ranking</a></li>
-          <li><a href="#">year</a></li>
-          <li><a href="#">ganle</a></li>
-        </ul>		
-      </div>
+    <div class="container">
+    <div class="header-title-area">
+    <h1 class="logo">アリスの映画紹介サイト</h1>
+    </div>
+      <ul class="header-navigation">
+        <li>
+          <a href="#">TOP</a>
+        </li>
+        <li>
+          <a href="#">ranking</a>
+        </li>
+        <li>
+          <a href="#">year</a>
+        </li>
+        <li>
+          <a href="#">genre</a>
+        </li>
+      </ul>
+    </div>
     <header>
-      <div class="main">
+    </header>
+    <div class="main">
       <div class="container">
         <div class="left-contents">
-            <div class="card-contents">
-                <div class="movie-list">
-                  <img src="./image/image" class="">
-                  <h2 class="text-title">バックインアクション</h2>
-                </div>
-                  <div class="teaser-date">
-                    <span class="year">2025</span>
-                    <span class="date">1/17</span>
-                  </div>
-              </div> 
-
-              <div class="card-contents">
-                <div class="teaser-title">
-                  <img src="./image/image2" class="">
-                  <h2 class="text-title">ミステリというなかれ</h2>
-                </div>
-                <div class="teaser-date">
-                  <span class="year">2023</span>
-                  <span class="date">9/15</span>
-                </div>
-              </div>
-
-                <div class="card-contents">
-                  <div class="teaser-title">
-                    <img src="./image/image3" class="">
-                    <h2 class="text-title">ジュラシックワールド</h2>
-                  </div>
-                    <span class="year">2015</span>
-                    <span class="date">8/5</span>
-                </div>
-                  
-
-              <div class="right-contents">
-                <div class="card-contents">
-                  <div class="teaser-title">
-                    <img src="./image/image4" class="">
-                      <h2 class="text-title">翔んで埼玉</h2>
-                  </div>
-                  <span class="year">2019</span>
-                  <span class="date">2/22</span>
-              </div>
-
-                <div class="card-contents">
-                <div class="teaser-title">
-                <img src="./image/image5" class="">
-                  <h2 class="text-title">死霊館</h2>
-                </div>
-                  <span class="year">2019</span>
-                  <span class="date">2/22</span>
-                </div>
-
-                <div class="card-contents">
-                <div class="teaser-title">
-                  <img src="./image/image6" class="">
-                  <h2 class="text-title">シンデレラ</h2>
-                </div>
-                  <span class="year">2015</span>
-                  <span class="date">4/25</span>
-                </div>
-              </div>
+          <div class="card-contents">
+            <div class="movie-list">
+              <div class="movie-img"><img src="{{ asset('image/image01.jpg') }}" class=""></div>
+              <h2 class="text-title">バックインアクション</h2>
+            </div>
+            <div class="teaser-date">
+              <span class="year">2025</span>
+              <span class="date">1/17</span>
+            </div>
           </div>
         </div>
+        
       </div>
+      
+          <div class="card-contents">
+            <div class="movie-list">
+              <div class="movie-img"><img src="{{ asset('image/image02.jpg') }}" class=""></div>
+              <h2 class="text-title">ミステリと言うなかれ</h2>
+            </div>
+            <div class="teaser-date">
+              <span class="year">2023</span>
+              <span class="date">9/15</span>
+            </div>
+          </div>
+      
+        
+          <div class="card-contents">
+            <div class="movie-list">
+              <div class="movie-img"><img src="{{ asset('image/image03.jpg') }}" class=""></div>
+              <h2 class="text-title">ジュラシックワールド</h2>
+            </div>
+            <div class="teaser-date">
+              <span class="year">2025</span>
+              <span class="date">1/17</span>
+            </div>
+          </div>
+        
+       
+          <div class="card-contents">
+            <div class="movie-list">
+              <div class="movie-img"><img src="{{ asset('image/image04.jpg') }}" class=""></div>
+              <h2 class="text-title">翔んで埼玉</h2>
+            </div>
+            <div class="teaser-date">
+              <span class="year">2019</span>
+              <span class="date">2/22</span>
+            </div>
+          </div>
+       
+          <div class="card-contents">
+            <div class="movie-list">
+              <div class="movie-img"><img src="{{ asset('image/image05.jpg') }}" class=""></div>
+              <h2 class="text-title">死霊館</h2>
+            </div>
+            <div class="teaser-date">
+              <span class="year">2019</span>
+              <span class="date">2/22</span>
+            </div>
+          </div>
+       
+          <div class="card-contents">
+            <div class="movie-list">
+              <div class="movie-img"><img src="{{ asset('image/image06.jpg') }}" class=""></div>
+              <h2 class="text-title">シンデレラ</h2>
+            </div>
+            <div class="teaser-date">
+              <span class="year">2015</span>
+              <span class="date">1/17</span>
+            </div>
+          </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
     </div>
   </body>
 </html>
