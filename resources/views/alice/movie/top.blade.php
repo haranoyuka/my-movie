7397bbc1885873096ec97b11f90e4d1f76b15692
EC2 Default User <ec2-user@ip-172-31-36-237.ap-northeast-1.compute.internal>
2025-06-22 09:12:35 +0000

    変更

--- resources/views/alice/movie/top.blade.php
+++ resources/views/alice/movie/top.blade.php
@@ -10,21 +10,21 @@
     <header>
       <div class="container">
       <div class="header-title-area">
-      <h1 id="logo">アリスの映画紹介サイト</h1>
+      <h1 class="logo">アリスの映画紹介サイト</h1>
       </div>
         <ul class="header-navigation">
           <li><a href="#">TOP</a></li>
-          <li><a href="#">ranking</a></li>
-          <li><a href="#">year</a></li>
-          <li><a href="#">genre</a></li>
+          <li><a href="#">Ranking</a></li>
+          <li><a href="#">Year</a></li>
+          <li><a href="#">Genre</a></li>
         </ul>
       </div>
     </header>
-    <fugure id="key">
+    <figure id="key">
+      <div id="key-in" class="wrapper">
+        <img src="{{ asset('image/top-key.jpg') }}" class="">
+      </div>
     </figure>
-    <div id="key-in" class="wrapper">
-      <img src="{{ asset('image/top-key.jpg') }}" class=""></div>
-    </div>
     <div class="main"> 
       <div class="wrapper">
         <div class="loops">
@@ -41,23 +41,24 @@
                   </span>
                 </div>
               </figure>
-                <span class="text-title">バックインアクション</span>
-                <div class="teaser-date">
-                  <span class="year">2025</span>
-                  <span class="date">1/17</span>
-                </div>
+              <span class="text-title">バックインアクション</span>
+              <div class="teaser-date">
+                <span class="year">2025</span>
+                <span class="date">1/17</span>
               </div>
+            </div>
             
               <div class="col x2 match" style="height 1377:">
                 <figure class="teaser-image">
                  <div class="popup">
-                 <span class="image">
-                   <div class="card-contents">
-                      <div class="movie-list">
-                        <div class="movie-img"><img src="{{ asset('image/image02.jpg') }}" class=""></div>
+                    <span class="image">
+                      <div class="card-contents">
+                        <div class="movie-list">
+                            <div class="movie-img"><img src="{{ asset('image/image02.jpg') }}" class=""></div>
+                          </div>
                       </div>
-                    </div>
-                  </span>
+                    </span>
+                  </div>
                 </figure>
                 <span class="text-title">ミステリと言うなかれ</span>      
                 <div class="teaser-date">
@@ -70,13 +71,13 @@
                 <figure class="teaser-image">
                   <div class="popup">
                    <span class="image">
-                    <div class="card-contents">
-                      <div class="movie-list">
-                        <div class="movie-img"><img src="{{ asset('image/image03.jpg') }}" class=""></div>
+                      <div class="card-contents">
+                        <div class="movie-list">
+                          <div class="movie-img"><img src="{{ asset('image/image003.png') }}" class=""></div>
                         </div>
                       </div>
-                    </div>
-                  </span> 
+                    </span> 
+                  </div> 
                 </figure>
                 <span class="text-title">ジュラシックワールド</span>     
                 <div class="teaser-date">
@@ -86,22 +87,22 @@
               </div>
 
               <div class="col x2 match" style="height 1377:">
-                    <figure class="teaser-image">
-                      <div class="popup">
-                        <span class="image">
-                          <div class="card-contents">
-                            <div class="movie-list">
-                              <div class="movie-img"><img src="{{ asset('image/image04.jpg') }}" class=""></div>
-                              </div>
-                            </div>
-                          </span> 
+                <figure class="teaser-image">
+                  <div class="popup">
+                    <span class="image">
+                      <div class="card-contents">
+                        <div class="movie-list">
+                          <div class="movie-img"><img src="{{ asset('image/image04.jpg') }}" class=""></div>
+                        </div>
                       </div>
-                    </figure>
-                    <span class="text-title">翔んで埼玉</span>
-                  <div class="teaser-date">
-                    <span class="year">2019</span>
-                    <span class="date">2/22</span>
+                    </span> 
                   </div>
+                </figure>
+                <span class="text-title">翔んで埼玉</span>
+                <div class="teaser-date">
+                  <span class="year">2019</span>
+                  <span class="date">2/22</span>
+                </div>
               </div>
             
               <div class="col x2 match" style="height 1377:">
@@ -129,7 +130,7 @@
                       <span class="image">
                         <div class="card-contents">
                           <div class="movie-list">
-                            <div class="movie-img"><img src="{{ asset('image/image06.jpg') }}" class=""></div>
+                            <div class="movie-img"><img src="{{ asset('image/image006.png') }}" class=""></div>
                           </div>
                         </div>
                       <span>
@@ -141,9 +142,36 @@
                     <span class="date">1/17</span>
                   </div>
                 </div>
-            </div>
+
+                <section id="profile">
+                  <div class="wrapper">
+                    <div class="d-flex">
+                      <div class="con-l">
+                        <h2 class="hd-main">PROFILE</h2>
+                          <dl class="d-flex">
+                            <dt>社名</dt>
+                            <dd>シロクラフト</dd>
+                            <dt>代表取締役</dt>
+                            <dd>真本英樹</dd>
+                            <dd>03-3505-5535</dd>
+                            <dd>contact</dd>
+                          </dl>
+                      </div>
+                      <div class="con-r">
+                        <div class="footer-img">
+                        <img src="{{ asset('image/image07.png') }}" class="map-image">  
+                      </div>
+                    </div>
+                  </div>
+                </section>
           </div>
         </div>
-     </div>
+      </div>
+    </div>      
+      <footer>
+        <div class="copyright">
+          <small> 2023 alice movie-intro </small>
+        </div>
+      </footer>
   </body>
 </html>
